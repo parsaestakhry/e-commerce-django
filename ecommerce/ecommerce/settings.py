9f917e3a3ca04b7050fb723574376c0373f3906b
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-22 20:55:23 +0330

    implemented mysql

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e-commerce',
+        'USER' : 'root',
+        'PASSWORD' : 'familyguyis@fox91',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        
     }
 }
 
