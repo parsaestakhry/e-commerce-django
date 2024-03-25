05c8ccc88d509d21ca4e628a3645c98164826824
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-25 17:41:15 +0330

    implemented models

--- ecommerce/api/admin.py
+++ ecommerce/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Purchase)
+admin.site.register(models.Product)
+admin.site.register(models.Manager)
+admin.site.register(models.Customer)
+admin.site.register(models.Category)
--- ecommerce/api/models.py
+++ ecommerce/api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=400)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    phone_number = models.BigIntegerField()
+    date_created = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.first_name + " - " + self.last_name
+    
+    
+class Purchase(models.Model):
+    id = models.IntegerField(primary_key=True)
+    amount = models.FloatField()
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    
+    def __str__(self) -> str:
+        return self.id + ":" + self.customer
+    
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.IntegerField(primary_key=True)
+    price = models.FloatField()
+    date_created = models.DateField(auto_now_add=True)
+    amount = models.IntegerField()
+    purchase = models.ManyToManyField(Purchase)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+
+
+
+class Manager(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length= 200)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+    
+    
+
+     
+
+    
+    
