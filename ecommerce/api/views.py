5865507be82ec13f1fc494867e3eaebb7a478ad8
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-26 12:37:00 +0330

    updated views

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from .models import Customer,Category,Product,Purchase,Manager
-from .serializers import CustomerSerializer
+from .serializers import CustomerSerializer,CategorySerializer
 from rest_framework.response import Response
 # Create your views here.
 
@@ -11,12 +11,42 @@ def hello(request):
 
 
 @api_view(['GET'])
-def getCustomer(request):
+def getCustomers(request):
     customers = Customer.objects.all()
     serializer = CustomerSerializer(customers, many=True)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
-def getCategory(request):
-    pass
+def getCategories(request):
+    categories = Category.objects.all()
+    serializer = CustomerSerializer(categories, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = CustomerSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getPurchases(request):
+    purchases = Purchase.objects.all()
+    serializer = CustomerSerializer(purchases, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getManagers(request):
+    managers = Manager.objects.all()
+    serializer = CustomerSerializer(managers, many=True)
+    return Response(serializer.data)
+
+
+
+
+
+
+
