bd2a16c50630a6e9f9d4c2bde697088d3fe958e1
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-26 13:02:43 +0330

    updated urls

--- ecommerce/api/urls.py
+++ ecommerce/api/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("get-managers/", views.getManagers, name="get-managers"),
     path("get-products/", views.getProducts, name="get-products"),
     path("get-purchases/", views.getPurchases, name="get-purchases"),
+    path("get-customer/<int:id>/", views.getSingleCustomer, name='get-customer'),
+    path("get-category/<int:id>/", views.getSingleCateogory, name='get-category'),
+    path("get-product/<int:id>/", views.getSingleProduct, name='get-product'),
+    path("get-purchase/<int:id>/", views.getSinglePurchase, name='get-purchase'),
+    path("get-manager/<int:id>/", views.getSingleManager, name='get-manager'),
+    
 ]
