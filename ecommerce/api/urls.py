d59df17d55db64950cc85d3f2ab544115cb5bf1d
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-26 12:43:18 +0330

    updated urls

--- ecommerce/api/urls.py
+++ ecommerce/api/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("hello/", views.hello, name="hello"),
-    path("get-customer/", views.getCustomers, name="get-customers")
+    path("", views.hello, name="hello"),
+    path("get-customers/", views.getCustomers, name="get-customers"),
+    path("get-categories/", views.getCategories, name="get-categories"),
+    path("get-managers/", views.getManagers, name="get-managers"),
+    path("get-products/", views.getProducts, name="get-products"),
+    path("get-purchases/", views.getPurchases, name="get-purchases"),
 ]
