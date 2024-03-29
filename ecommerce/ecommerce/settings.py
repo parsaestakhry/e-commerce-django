2aa84f32bc9e7eaa2d57cbb50d696deffcf1ca6e
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-29 13:55:44 +0330

    added user register app

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'crispy_forms',
-    'regitser',
+    'register',
 ]
 
 MIDDLEWARE = [
--- ecommerce/register/views.py
+++ ecommerce/register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
 # Create your views here.
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
