e8b269ed3970d2de027a2ff5a302ded3133e3ff3
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-29 17:36:40 +0330

    added user register and login

--- ecommerce/register/urls.py
+++ ecommerce/register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign-up/', views.registerUser, name='user-register')
+    path('sign-up/', views.registerUser, name='user-register'),
+    path('login/', views.authenticateUser, name='user-authentication')
 ]
--- ecommerce/register/views.py
+++ ecommerce/register/views.py
@@ -3,7 +3,8 @@ from .serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
 # Create your views here.
 @api_view(['POST'])
 def registerUser(request):
@@ -13,3 +14,14 @@ def registerUser(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def authenticateUser(request):
+    data = request.data
+    print(data['username'])
+    print(data['password'])
+    user = authenticate(username=data['username'], password=data['password'])
+    if user is not None:
+        return HttpResponse("hello")
+    return HttpResponse("no")
