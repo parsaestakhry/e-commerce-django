f720c607eceeebc6c15c402dd66abdfd806de30b
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-03-29 18:04:59 +0330

    user assigned to group at register

--- ecommerce/register/views.py
+++ ecommerce/register/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
+from django.contrib.auth.models import User,Group
 # Create your views here.
 @api_view(['POST'])
 def registerUser(request):
@@ -12,6 +13,15 @@ def registerUser(request):
     serializer = UserSerializer(data=data)
     if serializer.is_valid():
         serializer.save()
+        # print(serializer.data)
+        userdata = serializer.data
+        id = userdata['id']
+        user = User.objects.get(id=id)
+        # print(user)
+        user_group = Group.objects.get(name="customers")
+        user.groups.add(user_group)
+        # print(user)
+        # //print(username)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
