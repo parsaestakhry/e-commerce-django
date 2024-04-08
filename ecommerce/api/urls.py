7d19148a04687112443dcef8762b181b12a15c18
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-04-09 00:23:17 +0330

    changes in view function

--- ecommerce/api/urls.py
+++ ecommerce/api/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     # re_path(r'^api/products/category/(?P<category>\w+)/$', productCategoryList.as_view() , name='get_category_product'),
     # path('get-category-products/', views.getCategoryProducts, name='get-category-products'),
     path('get-category-products/', views.productList.as_view(), name='hello'),
-    path('get-category-id/<str:category>/' , views.getCategoryId, name='get-category')
+    path('get-category-id/<str:category>/' , views.getCategoryIdProducts, name='get-category')
     
     
     
